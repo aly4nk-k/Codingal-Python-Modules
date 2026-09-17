4227c45c209d6271a5b5278d363ab65c0906a6d2
Alyan Kabir <aly4n.K@gmail.com>
2026-09-17 19:14:06 +0400

    finished m7 l1 a1

---
+++ Module 7 - Python Intro/Lesson 1/activity-1.py
@@ -0,0 +1,47 @@
+print("My Name is Alyan Kabir")
+print("The Time Here is 6:49 PM")
+print("I love Coding")
+print("1234 + 34567")
+print(12345 + 34567)
+print(10000 - 5000)
+
+name = "Alyan"
+print(name)
+
+age = 14
+print(age)
+
+x = 78
+y = 100
+print(x * y)
+
+money = 150
+
+# convert money to US dollars 
+print(150 / 95)
+
+# convert to UAE Dirhams
+print(150 / 26)
+
+print(money, "Indian Rupees =", round(money / 95,2), "US Dollars")
+
+print(money, "Indian Rupees =", round(money / 26,2), "UAE Dirhams")
+
+# f-string
+print(f"{money} Indian Rupees = {round(money/95,2)} US Dollars")
+
+# 3 RULES WHEN MAKING A VARIABLE
+# RULE 1 : Cannot start with a number
+# RULE 2 : Only Characters allwoed are alphabets, numbers, and underscore
+my_1st_pet_dog = "Oreo"
+print(my_1st_pet_dog)
+# RULE 3 : You cannot use Reserved Keywords for Variable Names
+import keyword
+print(keyword.kwlist)
+
+# Print function always starts on a new line
+print("Step 1")
+print("Step 2")
+print("Step 3")
+
+print("Step 4\nStep 5\nStep 6")
